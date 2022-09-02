2d78e6bdeef305211a1df67f64f1e96e14603b8c
amaljom <amaljom99@gmail.com>
2022-09-02 17:08:16 +0200

    create function ed una nuova istanza

--- index.php
+++ index.php
@@ -1,15 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php 
- class movies{
+ class Movie{
 
-    private $name;
-    private $releaseDate;
-    private $director;
-    private $genre;
- 
+    public $name;
+    public $releaseYear;
+    public $director;
+    public $genre;
+
+    function __constructor($name, $releaseYear, $director, $genre){
+        $this->name = $name;
+        $this->releaseYear = $releaseYear;
+        $this->director = $director;
+        $this->genre = $genre;
+    }
+    function get_name(){
+        return $this-> name;
+    }
+    
+    function get_releaseYear(){
+        return $this->releaseYear;
+    }
+    
+    function get_director(){
+        return $this->director;
+    }
+    
+    function get_genre(){
+        return $this->genre;
+    }
 }
 
-
-
-
+$movieOne = new Movie("interstellar", "2018", "Nolan", "Sci-fi");
+var_dump($movieOne);
 
 ?>
+
+</body>
+</html>
